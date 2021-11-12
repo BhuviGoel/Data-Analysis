7bef9fce71d6c18d28a314b5d1a4d473c4d883f7
BhuviGoel <73011205+BhuviGoel@users.noreply.github.com>
2021-11-12 17:38:35 +0530

    Class 107

---
+++ mean.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import csv
+import plotly.graph_objects as bo
+
+data = pd.read_csv(r"C:\Users\bhuvi\Google Drive\Code\Python\Class 107 - Data Analysis\data.csv")
+
+mean_std = data.groupby("level")["attempt"].mean()
+fig = bo.Figure(bo.Bar(x = ["Level 1","Level 2","Level 3","Level 4"], y =mean_std , orientation = 'v' ))
+fig.show()
